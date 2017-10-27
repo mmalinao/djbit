ce11ba468600142332951480ae8d1c968f95f6cc
Matt Malinao <matt.malinao@gmail.com>
2017-10-26 22:58:14 -0600

    sign in user on slack callback

--- lib/djbit_web/controllers/session_controller.ex
+++ lib/djbit_web/controllers/session_controller.ex
@@ -1,9 +1,13 @@
 defmodule DjBitWeb.SessionController do
   use DjBitWeb, :controller
   alias DjBit.Accounts
+  alias DjBitWeb.Guardian
 
   def callback(conn, %{"code" => code}) do
-    {:ok, _user} = Accounts.create_user_from_oauth(code)
-    text conn, "OK"
+    {:ok, user} = Accounts.create_user_from_oauth(code)
+
+    conn
+    |> Guardian.Plug.sign_in(user)
+    |> text("OK")
   end
 end
--- test/djbit_web/controllers/session_controller_test.exs
+++ test/djbit_web/controllers/session_controller_test.exs
@@ -6,6 +6,7 @@ defmodule DjBitWeb.SessionControllerTest do
 
   alias DjBit.Repo
   alias DjBit.Accounts.User
+  alias DjBitWeb.Guardian
 
   describe "GET /slack/callback" do
     test "creates a User", %{conn: conn} do
@@ -18,6 +19,18 @@ defmodule DjBitWeb.SessionControllerTest do
       end
 
       assert Repo.get_by(User, slack_id: slack_user_identity["user"]["id"])
+    end
+
+    test "signs in user", %{conn: conn} do
+      slack_user_identity = string_params_for(:slack_user_identity)
+
+      with_mock DjBit.Slack, [exchange_for_user_identity: fn("valid_code") -> slack_user_identity end] do
+        resp = conn
+        |> get("/slack/callback", %{"code" => "valid_code"})
+
+        assert Guardian.Plug.authenticated?(resp)
+        assert Guardian.Plug.current_resource(resp).slack_id == slack_user_identity["user"]["id"]
+      end
     end
   end
 end
