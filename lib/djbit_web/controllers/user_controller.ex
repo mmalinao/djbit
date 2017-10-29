43754d18d1a3245194570ef5a12fb759d25e5c30
Matt Malinao <matt.malinao@gmail.com>
2017-10-28 18:14:45 -0600

    bump version 0.1.0

--- lib/djbit_web/controllers/user_controller.ex
+++ lib/djbit_web/controllers/user_controller.ex
@@ -2,8 +2,6 @@ defmodule DjBitWeb.UserController do
   use DjBitWeb, :controller
   alias DjBit.Accounts
 
-  action_fallback DjBitWeb.FallbackController
-
   def show(conn, _params) do
     render conn, "show.html"
   end
